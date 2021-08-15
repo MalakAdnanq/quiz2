58b2b4cb269c90949fe25d96052c46642c0d4d7f
Malak <malak.adnan.q@gmail.com>
2021-08-15 15:25:02 +0300

    Initial commit

---
+++ JS/JS.js
@@ -0,0 +1,16 @@
+function {
+var y = document.getElementById('chid')
+}
+
+function yesover(){
+var y = document.getElementById('yesid')
+y.innerHTML= 'lets do'
+} 
+
+function yesout(){
+var y = document.getElementById('yesid')
+y.innerHTML= 'yes'
+}
+
+
+
